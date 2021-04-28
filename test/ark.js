068604360da1656d89074674a6f5dccd9ea93e88
Maxime Dupré <maximedupre@me.com>
2021-04-28 11:25:47 -0400

    feat: should make sure that the supply is 1.5 bil

--- test/ark.js
+++ test/ark.js
@@ -1,11 +1,19 @@
 const ARK = artifacts.require('ARK');
 
 contract('ARK', accounts => {
-    let instance;
-
     it('should be able to deploy an instance', async () => {
         await ARK.deployed();
 
         return assert.isTrue(true);
+    });
+
+    it('should have a 1.5B token supply', async () => {
+        const instance = await ARK.deployed();
+
+        const supply = await instance.totalSupply();
+
+        const decimals = await instance.decimals();
+
+        assert.equal(1500000000, +supply.toString() / 10 ** decimals);
     });
 });
