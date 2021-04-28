3222cbc0be705303d2107415f324884cc2400dcc
Maxime Dupré <maximedupre@me.com>
2021-04-28 10:20:46 -0400

    sets the initial supply

--- contracts/ARK.sol
+++ contracts/ARK.sol
@@ -4,7 +4,7 @@ pragma solidity 0.8.4;
 import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
 
 contract ARKToken is ERC20 {
-    constructor(uint256 initialSupply) ERC20('Arkitects', 'ARK') {
-        _mint(msg.sender, initialSupply);
+    constructor() ERC20('Arkitects', 'ARK') {
+        _mint(msg.sender, 1500000000);
     }
 }
