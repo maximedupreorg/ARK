3f575b4defef4e295e06d0b289eafcac5345982b
Maxime Dupré <maximedupre@me.com>
2021-04-29 11:47:47 -0400

    adds a test to verify fee distribution

--- test/ark.js
+++ test/ark.js
@@ -21,4 +21,39 @@ contract('ARK', accounts => {
         assert.equal(1500000000, +supply.toString() / decimals);
         assert.equal(1500000000, +mainAccountBalance.toString() / decimals);
     });
+
+    it('should be able to transfer tokens to a second user and have fee distribution', async () => {
+        const instance = await ARK.deployed();
+
+        const mainAccountBalance = await instance.balanceOf(accounts[0]);
+
+        const transferAmount = mainAccountBalance.divn(2);
+
+        await instance.transfer(accounts[1], transferAmount);
+
+        const mainAccountNewBalance = await instance.balanceOf(accounts[0]);
+
+        const secondAccountNewBalance = await instance.balanceOf(accounts[1]);
+
+        const feeAmount = transferAmount.divn(20);
+        const expectedNewMainAccountBalance = mainAccountBalance
+            .sub(transferAmount)
+            .add(feeAmount.divn(2));
+        const expectedSecondAccountBalance = secondAccountNewBalance
+            .sub(transferAmount)
+            .sub(feeAmount.divn(2));
+
+        console.log(expectedNewMainAccountBalance.toString());
+        console.log(mainAccountNewBalance.toString());
+        console.log(feeAmount.toString());
+
+        assert.equal(
+            expectedNewMainAccountBalance.toString(),
+            mainAccountNewBalance.toString(),
+        );
+        assert.equal(
+            expectedSecondAccountBalance.toString(),
+            secondAccountNewBalance.toString(),
+        );
+    });
 });
