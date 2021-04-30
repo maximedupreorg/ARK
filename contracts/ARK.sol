d865d71906c51000125390a11479126d0b128c88
Maxime Dupré <maximedupre@me.com>
2021-04-30 09:45:30 -0400

    adds the functions that complete the balanceof function chain

--- contracts/ARK.sol
+++ contracts/ARK.sol
@@ -4,8 +4,11 @@ pragma solidity 0.8.4;
 import '@openzeppelin/contracts/utils/Context.sol';
 import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
 import '@openzeppelin/contracts/access/Ownable.sol';
+import '@openzeppelin/contracts/utils/math/SafeMath.sol';
 
 contract ARK is Context, IERC20, Ownable {
+    using SafeMath for uint256;
+
     uint256 private constant MAX = ~uint256(0);
     uint256 private constant _tTotal = 10 * 10**6 * 10**9;
 
@@ -14,6 +17,9 @@ contract ARK is Context, IERC20, Ownable {
     uint8 private _decimals = 9;
     uint256 private _rTotal = (MAX - (MAX % _tTotal));
     mapping(address => uint256) private _rOwned;
+    mapping(address => uint256) private _tOwned;
+    mapping(address => bool) private _isExcluded;
+    address[] private _excluded;
 
     constructor() {
         _rOwned[_msgSender()] = _rTotal;
@@ -35,5 +41,57 @@ contract ARK is Context, IERC20, Ownable {
 
     function totalSupply() public view override returns (uint256) {
         return _tTotal;
+    }
+
+    ///////////// BALANCEOF CHAIN /////////////
+
+    function balanceOf(address account) public view override returns (uint256) {
+        if (_isExcluded[account]) return _tOwned[account];
+
+        return tokenFromReflection(_rOwned[account]);
+    }
+
+    function tokenFromReflection(uint256 rAmount)
+        public
+        view
+        returns (uint256)
+    {
+        require(
+            rAmount <= _rTotal,
+            'Amount must be less than total reflections'
+        );
+
+        uint256 currentRate = _getRate();
+
+        return rAmount.div(currentRate);
+    }
+
+    function _getRate() private view returns (uint256) {
+        (uint256 rSupply, uint256 tSupply) = _getCurrentSupply();
+
+        return rSupply.div(tSupply);
+    }
+
+    function _getCurrentSupply() private view returns (uint256, uint256) {
+        uint256 rSupply = _rTotal;
+        uint256 tSupply = _tTotal;
+
+        for (uint256 i = 0; i < _excluded.length; i++) {
+            if (
+                _rOwned[_excluded[i]] > rSupply ||
+                _tOwned[_excluded[i]] > tSupply
+            ) {
+                return (_rTotal, _tTotal);
+            }
+
+            rSupply = rSupply.sub(_rOwned[_excluded[i]]);
+            tSupply = tSupply.sub(_tOwned[_excluded[i]]);
+        }
+
+        if (rSupply < _rTotal.div(_tTotal)) {
+            return (_rTotal, _tTotal);
+        }
+
+        return (rSupply, tSupply);
     }
 }
