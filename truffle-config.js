c9a63f0a2e1fa7b7c13a3080d035fd8b71e03c5b
Maxime Dupré <maximedupre@me.com>
2021-05-04 22:14:14 -0400

    adds main net deployment info

--- truffle-config.js
+++ truffle-config.js
@@ -1,7 +1,21 @@
 const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+
+const web3 = new Web3();
+
+console.log(process.env.ARK_MNEMONICS_MAINNET);
 
 module.exports = {
     networks: {
+        mainnet: {
+            provider: () =>
+                new HDWalletProvider({
+                    mnemonic: process.env.ARK_MNEMONICS_MAINNET,
+                    providerOrUrl: `https://eth-mainnet.alchemyapi.io/v2/${process.env.ARK_ALCHEMY_API_KEY_MAINNET}`,
+                }),
+            network_id: 1,
+            gasPrice: web3.utils.toWei('40', 'gwei'),
+        },
         rinkeby: {
             provider: () =>
                 new HDWalletProvider({
